a4eb977c4ed53514d52159e4438f48f3f99f704c
oinari421 <kaitoangle@gmail.com>
2026-02-18 23:59:44 +0900

    Update game.js

--- game.js
+++ game.js
@@ -654,8 +654,8 @@ y = baseTop  + Math.cos(t * 1.3) * 30 * ramp;
 
   }else if (currentStage === 10) {
 
-  const CHANGE_INTERVAL = 0.8;   // 位置変更間隔（小さいほど暴れる）
-  const MOVE_SPEED = 3.0;        // 移動速度（大きいほど俊敏）
+  const CHANGE_INTERVAL = 0.7;   // 位置変更間隔（小さいほど暴れる）
+  const MOVE_SPEED = 4.5;        // 移動速度（大きいほど俊敏）
 
   if (!target._rand) {
     target._rand = {
@@ -773,6 +773,7 @@ setScreen("boot");
 
 
 })();
+
 
 
 
